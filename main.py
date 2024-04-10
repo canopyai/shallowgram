1620f3d6a4c1d6e4548fd468f4fc3ebaeb54b362
Amartya Varma <amartya.varma@gmail.com>
2024-04-10 10:52:15 +0100

    all broadcast

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ from longer_than_one_word import is_longer_than_one_word
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-
 vad_model, vad_utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
                                       model='silero_vad',
                                       force_reload=True)
-(get_speech_timestamps, save_audio, read_audio, VADIterator, collect_chunks) = vad_utils
+(get_speech_timestamps, save_audio, read_audio,
+ VADIterator, collect_chunks) = vad_utils
 
 vad_model = vad_model.to(device)
 
 print("Models loaded")
+
 
 def int2float(sound):
     abs_max = np.abs(sound).max()
@@ -30,19 +31,20 @@ def int2float(sound):
     sound = sound.squeeze()
     return sound
 
+
 BUFFER_SIZE = 1000
 audio_buffer = np.array([], dtype=np.float32)
-accumulated_audio = np.array([], dtype=np.float32)  
+accumulated_audio = np.array([], dtype=np.float32)
 full_accumulated_audio = np.array([], dtype=np.float32)
 last_confidence = 0
 processed_time_ms = 0
 total_length_ms = 0
 confidence_threshold = 0.5
-clients = set() 
+clients = set()
 
 
-#create a random id for each frame 
-# call the transcribe function and the text attached to that id should be processed 
+# create a random id for each frame
+# call the transcribe function and the text attached to that id should be processed
 
 
 async def audio_processor(websocket, path):
@@ -52,37 +54,62 @@ async def audio_processor(websocket, path):
         async for packet in websocket:
             audio_int16 = np.frombuffer(packet, np.int16)
             audio_float32 = int2float(audio_int16)
-            
+
             audio_buffer = np.concatenate((audio_buffer, audio_float32))
-            accumulated_audio = np.concatenate((accumulated_audio, audio_float32))  # Accumulate audio data
-            full_accumulated_audio = np.concatenate((full_accumulated_audio, audio_float32))
+            accumulated_audio = np.concatenate(
+                (accumulated_audio, audio_float32))  # Accumulate audio data
+            full_accumulated_audio = np.concatenate(
+                (full_accumulated_audio, audio_float32))
             packet_duration_ms = (len(audio_float32) / 16000) * 1000
             total_length_ms += packet_duration_ms
-            
+
             while len(audio_buffer) >= BUFFER_SIZE:
-                tensor_audio = torch.from_numpy(audio_buffer[:BUFFER_SIZE]).unsqueeze(0)
+                tensor_audio = torch.from_numpy(
+                    audio_buffer[:BUFFER_SIZE]).unsqueeze(0)
                 tensor_audio = tensor_audio.to(device)
-                confidence = vad_model(tensor_audio, 16000).item()  # Use the VAD model for voice activity detection
+                # Use the VAD model for voice activity detection
+                confidence = vad_model(tensor_audio, 16000).item()
 
                 processed_time_ms += (BUFFER_SIZE / 16000) * 1000
-                
+
                 if last_confidence > confidence_threshold and confidence < confidence_threshold:
+                    await websocket.send(json.dumps({
+                        "messageType": "vad",
+                        "data": {
+                            "vad_type":"end", 
+                        }
+                    }))
                     if len(accumulated_audio) > 0:  # Ensure there's audio to save
-                        transcription, inference_time = transcribe(accumulated_audio)
-                        if(is_longer_than_one_word(transcription)):
+                        transcription, inference_time = transcribe(
+                            accumulated_audio)
+                        if (is_longer_than_one_word(transcription)):
                             await websocket.send(json.dumps({
-                                "transcription": transcription,
-                                "inference_time": inference_time
-                                }))  # Send transcription back to client
+                                "message_type": "transcription",
+                                "data": {
+                                    "transcription": transcription,
+                                    "inference_time": inference_time
+                                }
+
+                            }))  # Send transcription back to client
                         # await websocket.send(fulltranscription)
-               
-                        accumulated_audio = np.array([], dtype=np.float32)  # Reset accumulation buffer
+
+                        # Reset accumulation buffer
+                        accumulated_audio = np.array([], dtype=np.float32)
+
+                elif last_confidence < confidence_threshold and confidence > confidence_threshold:
+                    await websocket.send(json.dumps({
+                        "messageType": "vad",
+                        "data": {
+                            "vad_type":"start", 
+                        }
+                    }))
 
                 last_confidence = confidence
                 audio_buffer = audio_buffer[BUFFER_SIZE:]
 
     except exceptions.ConnectionClosed:
         print("Connection closed")
+
 
 async def main():
     async with websockets.serve(audio_processor, "0.0.0.0", 8080):
