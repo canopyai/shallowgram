3a6d7846c3a0513c1722d651c079838f106d3a30
Amartya Varma <amartya.varma@gmail.com>
2024-04-13 16:47:30 +0100

    s

--- audio_emotion/get_emotion_data.py
+++ audio_emotion/get_emotion_data.py
@@ -2,6 +2,7 @@ import numpy as np
 from audio_emotion.post_numpy_array import post_numpy_array
 import requests
 import io
+import time
 
 
 endpoint = "http://34.90.83.54:8080"
@@ -9,6 +10,7 @@ endpoint = "http://34.90.83.54:8080"
 
 def get_emotion_data(numpy_array):
 
+    startTime = time.time()
     # Convert the numpy_array from float32 to int16
     int16_audio = (numpy_array * 32767).astype(np.int16)  # Scale float32 to int16 range
 
@@ -17,4 +19,6 @@ def get_emotion_data(numpy_array):
     files = {'audio': ('audio_data', io.BytesIO(audio_bytes), 'application/octet-stream')}
     aud_emo_data = requests.post(endpoint, files=files)
     resp_json = aud_emo_data.json()
+    endTime = time.time()
+    print(f'duration is for audio int {endTime - startTime}: {resp_json}')
     return resp_json
